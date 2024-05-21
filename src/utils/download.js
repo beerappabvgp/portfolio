958ef12acaa6d76f1a96d5983365ab301a98c81c
beerappabvgp <beerappabharathb@gmail.com>
2024-05-21 08:47:23 +0530

    updated resume link

--- src/utils/download.js
+++ src/utils/download.js
@@ -1,6 +1,6 @@
 export const handleDownload = () => {
     const link = document.createElement('a');
-    link.href = "https://drive.google.com/uc?id=1j-uQy_1cZg7BN2kTBsUgkjIgoptNVWCl";
+    link.href = "https://drive.google.com/uc?id=15U8fvxRWzFas8S6T_4YbM7KOwYTKZ2tP";
     link.download = 'resume.pdf'; 
     link.click();
 }
